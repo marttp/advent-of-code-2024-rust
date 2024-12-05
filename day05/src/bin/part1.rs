2a0ebf40462a98682b00c9f20ffdfd45391666fd
Thanaphoom Babparn <tp.babparn@gmail.com>
2024-12-05 15:49:59 +0900

    Day 05 - Part 1

---
+++ day05/src/bin/part1.rs
@@ -0,0 +1,64 @@
+mod common;
+
+use std::collections::{HashMap, HashSet};
+use common::split_lines;
+
+fn main() {
+    let input = include_str!("./input5.txt");
+    let output = solution(split_lines(input));
+    dbg!(output);
+}
+
+fn solution(input: Vec<&str>) -> i32 {
+    let mut rules: Vec<&str> = Vec::new();
+    let mut updates: Vec<&str> = Vec::new();
+    let mut is_received_update: bool = false;
+    for line in input {
+        match line.is_empty() {
+            true => is_received_update = true,
+            _ => {
+                if is_received_update {
+                    updates.push(line);
+                } else {
+                    rules.push(line);
+                }
+            }
+        }
+    }
+
+    let mut rule_map: HashMap<i32, HashSet<i32>> = HashMap::new();
+    for rule in rules {
+        let order_rule: Vec<i32> = rule.split('|')
+            .map(|n| n.parse::<i32>().unwrap())
+            .collect();
+        rule_map.entry(order_rule[0])
+            .or_insert(HashSet::new())
+            .insert(order_rule[1]);
+    }
+
+    let updates: Vec<Vec<i32>> = updates.into_iter()
+        .map(|line| line.split(",").map(|x| x.parse::<i32>().unwrap()).collect())
+        .collect();
+
+    updates.iter().filter(|u| valid_update(u, &rule_map))
+        .map(|u| middle_element(u))
+        .sum()
+}
+
+fn valid_update(u: &Vec<i32>, rule_map: &HashMap<i32, HashSet<i32>>) -> bool {
+    for i in 0..u.len() {
+        if let Some(rule_set) = rule_map.get(&u[i]) {
+            for j in 0..i {
+                if rule_set.contains(&u[j]) {
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
+}
+
+fn middle_element(u: &Vec<i32>) -> i32 {
+    let length = u.len();
+    u[length/2]
+}
