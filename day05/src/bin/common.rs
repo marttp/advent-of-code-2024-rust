507f7a46e4a44013d963d8318b80c34d9a041902
Thanaphoom Babparn <tp.babparn@gmail.com>
2024-12-05 16:03:47 +0900

    Day 05 - Extract to common function

--- day05/src/bin/common.rs
+++ day05/src/bin/common.rs
@@ -1,8 +1,10 @@
+use std::collections::{HashMap, HashSet};
+
 pub fn split_lines(input: &str) -> Vec<&str> {
     input.split("\n").collect()
 }
 
-pub fn extract_rules_updates(input: Vec<&str>) -> (Vec<&str>, Vec<&str>) {
+pub fn extract_rules_updates(input: Vec<&str>) -> (Vec<&str>, Vec<&str>, HashMap<i32, HashSet<i32>>) {
     let mut rules: Vec<&str> = Vec::new();
     let mut updates: Vec<&str> = Vec::new();
     let mut is_received_update: bool = false;
@@ -18,5 +20,33 @@ pub fn extract_rules_updates(input: Vec<&str>) -> (Vec<&str>, Vec<&str>) {
             }
         }
     }
-    (rules, updates)
+    let mut rule_map: HashMap<i32, HashSet<i32>> = HashMap::new();
+    for rule in rules.clone() {
+        let order_rule: Vec<i32> = rule.split('|')
+            .map(|n| n.parse::<i32>().unwrap())
+            .collect();
+        rule_map.entry(order_rule[0])
+            .or_insert(HashSet::new())
+            .insert(order_rule[1]);
+    }
+
+    (rules, updates, rule_map)
+}
+
+pub fn valid_update(u: &Vec<i32>, rule_map: &HashMap<i32, HashSet<i32>>) -> bool {
+    for i in 0..u.len() {
+        if let Some(rule_set) = rule_map.get(&u[i]) {
+            for j in 0..i {
+                if rule_set.contains(&u[j]) {
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
+}
+
+pub fn middle_element(u: &Vec<i32>) -> i32 {
+    let length = u.len();
+    u[length/2]
 }
--- day05/src/bin/part1.rs
+++ day05/src/bin/part1.rs
@@ -1,7 +1,6 @@
 mod common;
 
-use std::collections::{HashMap, HashSet};
-use common::{split_lines, extract_rules_updates};
+use common::{extract_rules_updates, middle_element, split_lines, valid_update};
 
 fn main() {
     let input = include_str!("./input5.txt");
@@ -10,16 +9,7 @@ fn main() {
 }
 
 fn solution(input: Vec<&str>) -> i32 {
-    let (rules, updates) = extract_rules_updates(input.clone());
-    let mut rule_map: HashMap<i32, HashSet<i32>> = HashMap::new();
-    for rule in rules {
-        let order_rule: Vec<i32> = rule.split('|')
-            .map(|n| n.parse::<i32>().unwrap())
-            .collect();
-        rule_map.entry(order_rule[0])
-            .or_insert(HashSet::new())
-            .insert(order_rule[1]);
-    }
+    let (_, updates, rule_map) = extract_rules_updates(input.clone());
 
     let updates: Vec<Vec<i32>> = updates.into_iter()
         .map(|line| line.split(",").map(|x| x.parse::<i32>().unwrap()).collect())
@@ -28,22 +18,4 @@ fn solution(input: Vec<&str>) -> i32 {
     updates.iter().filter(|u| valid_update(u, &rule_map))
         .map(|u| middle_element(u))
         .sum()
-}
-
-fn valid_update(u: &Vec<i32>, rule_map: &HashMap<i32, HashSet<i32>>) -> bool {
-    for i in 0..u.len() {
-        if let Some(rule_set) = rule_map.get(&u[i]) {
-            for j in 0..i {
-                if rule_set.contains(&u[j]) {
-                    return false;
-                }
-            }
-        }
-    }
-    return true;
-}
-
-fn middle_element(u: &Vec<i32>) -> i32 {
-    let length = u.len();
-    u[length/2]
 }
