2be253dcd7580f6c15e1176b875fa1b2aa66e6b3
Thanaphoom Babparn <tp.babparn@gmail.com>
2024-12-18 19:28:55 +0900

    Day 18 - Part 2

---
+++ day18/src/bin/part2.rs
@@ -0,0 +1,34 @@
+mod common;
+
+use crate::common::{shortest_path, split_lines};
+
+fn main() {
+    let input = include_str!("input18.txt");
+    let output = solution(split_lines(input), 70, 70);
+    dbg!(output);
+}
+
+fn solution(input: Vec<&str>, m: usize, n: usize) -> String {
+    let mut bytes: usize = 1;
+    let size = input.len();
+
+    while bytes < size {
+        let result = shortest_path(input.clone(), m, n, bytes);
+        if result == 0 {
+            return input[bytes - 1].to_string();
+        }
+        bytes += 1;
+    }
+
+    "0,0".to_string()
+}
+
+#[allow(dead_code)]
+fn display(grid: &Vec<Vec<char>>) {
+    for row in grid.iter() {
+        for cell in row.iter() {
+            print!("{}", cell);
+        }
+        println!();
+    }
+}
