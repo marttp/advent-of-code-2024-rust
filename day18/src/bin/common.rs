907355e1d5af37679ddf753bfa18b89a916cf4cb
Thanaphoom Babparn <tp.babparn@gmail.com>
2024-12-18 19:23:55 +0900

    Day 18 - Part 1 refactor

--- day18/src/bin/common.rs
+++ day18/src/bin/common.rs
@@ -1,3 +1,53 @@
+use std::collections::VecDeque;
+
+const EMPTY: char = '.';
+const OBSTACLE: char = '#';
+
+const DIRECTIONS: [(i32, i32); 4] = [(-1, 0), (1, 0), (0, -1), (0, 1)];
+
 pub fn split_lines(input: &str) -> Vec<&str> {
     input.split("\n").collect()
+}
+
+pub fn shortest_path(input: Vec<&str>, m: usize, n: usize, byte_take: usize) -> u64 {
+    let m = m + 1;
+    let n = n + 1;
+    let mut grid = vec![vec![EMPTY; n]; m];
+
+    for line in input.clone().into_iter().take(byte_take) {
+        let coord = line.split(",").collect::<Vec<&str>>();
+        let c = coord[0].parse::<usize>().unwrap();
+        let r = coord[1].parse::<usize>().unwrap();
+        grid[r][c] = OBSTACLE;
+    }
+
+    let start = (0, 0);
+    let end = (m - 1, n - 1);
+    let mut visited = vec![vec![false; n]; m];
+    let mut queue: VecDeque<((usize, usize), u64)> = VecDeque::new();
+    queue.push_back((start, 0));
+
+    let mut final_distance = 0;
+    while let Some(((r, c), distance)) = queue.pop_front() {
+        if (r == end.0) && (c == end.1) {
+            final_distance = distance;
+            break;
+        }
+        for (dr, dc) in DIRECTIONS.iter() {
+            let nr = r as i32 + dr;
+            let nc = c as i32 + dc;
+            if nr < 0
+                || nr >= m as i32
+                || nc < 0
+                || nc >= n as i32
+                || grid[nr as usize][nc as usize] == OBSTACLE
+                || visited[nr as usize][nc as usize]
+            {
+                continue;
+            }
+            visited[nr as usize][nc as usize] = true;
+            queue.push_back(((nr as usize, nc as usize), distance + 1));
+        }
+    }
+    final_distance
 }
--- day18/src/bin/part1.rs
+++ day18/src/bin/part1.rs
@@ -1,54 +1,15 @@
 mod common;
 
-use crate::common::split_lines;
-use std::collections::VecDeque;
-
-const EMPTY: char = '.';
-const OBSTACLE: char = '#';
-
-const DIRECTIONS: [(i32, i32); 4] = [(-1, 0), (1, 0), (0, -1), (0, 1)];
+use crate::common::{shortest_path, split_lines};
 
 fn main() {
     let input = include_str!("input18.txt");
-    let output = solution(split_lines(input), 70, 70, 1024);
+    let output = solution(split_lines(input), 70, 70);
     dbg!(output);
 }
 
-fn solution(input: Vec<&str>, m: usize, n: usize, byte_take: usize) -> u64 {
-    let m = m + 1;
-    let n = n + 1;
-    let mut grid = vec![vec![EMPTY; n]; m];
-
-    for line in input.clone().into_iter().take(byte_take) {
-        let coord = line.split(",").collect::<Vec<&str>>();
-        let c = coord[0].parse::<usize>().unwrap();
-        let r = coord[1].parse::<usize>().unwrap();
-        grid[r][c] = OBSTACLE;
-    }
-
-    let start = (0, 0);
-    let end = (m - 1, n - 1);
-    let mut visited = vec![vec![false; n]; m];
-    let mut queue: VecDeque<((usize, usize), u64)> = VecDeque::new();
-    queue.push_back((start, 0));
-
-    let mut final_distance = 0;
-    while let Some(((r, c), distance)) = queue.pop_front() {
-        if (r == end.0) && (c == end.1) {
-            final_distance = distance;
-            break;
-        }
-        for (dr, dc) in DIRECTIONS.iter() {
-            let nr = r as i32 + dr;
-            let nc = c as i32 + dc;
-            if nr < 0 || nr >= m as i32 || nc < 0 || nc >= n as i32 || grid[nr as usize][nc as usize] == OBSTACLE || visited[nr as usize][nc as usize] {
-                continue;
-            }
-            visited[nr as usize][nc as usize] = true;
-            queue.push_back(((nr as usize, nc as usize), distance + 1));
-        }
-    }
-    final_distance
+fn solution(input: Vec<&str>, m: usize, n: usize) -> u64 {
+    shortest_path(input, m, n, 1024)
 }
 
 #[allow(dead_code)]
